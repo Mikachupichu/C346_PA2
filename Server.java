3ef966505c89c24859c295951db4bce1d7950826
“Mikachupichu” <michael.pouget01@gmail.com>
2025-03-07 20:09:19 -0500

    Final Submission

--- Server.java
+++ Server.java
@@ -358,7 +358,7 @@ public class Server extends Thread {
                         } 
                 } 
             
-                System.out.println("\n DEBUG : Server.deposit - " + "i " + i + " Current balance " + curBalance + " Amount " + amount + " " + getServerThreadId());
+                // System.out.println("\n DEBUG : Server.deposit - " + "i " + i + " Current balance " + curBalance + " Amount " + amount + " " + getServerThreadId());
             
                 account[i].setBalance(curBalance + amount);     /* Deposit amount in the account */
                 return account[i].getBalance ();                /* Return updated account balance */
@@ -378,7 +378,7 @@ public class Server extends Thread {
         synchronized(account) {
             curBalance = account[i].getBalance( );          /* Get current account balance */
             
-            System.out.println("\n DEBUG : Server.withdraw - " + "i " + i + " Current balance " + curBalance + " Amount " + amount + " " + getServerThreadId());
+            // System.out.println("\n DEBUG : Server.withdraw - " + "i " + i + " Current balance " + curBalance + " Amount " + amount + " " + getServerThreadId());
             
             account[i].setBalance(curBalance - amount);     /* Withdraw amount in the account */
             return account[i].getBalance ();                /* Return updated account balance */
@@ -399,7 +399,7 @@ public class Server extends Thread {
         synchronized(account) {
             curBalance = account[i].getBalance( );          /* Get current account balance */
             
-            System.out.println("\n DEBUG : Server.query - " + "i " + i + " Current balance " + curBalance + " " + getServerThreadId()); 
+            // System.out.println("\n DEBUG : Server.query - " + "i " + i + " Current balance " + curBalance + " " + getServerThreadId()); 
             
             return curBalance;                              /* Return current account balance */
         }
